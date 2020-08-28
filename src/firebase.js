0411729502cf5aaadb6f5e90373cf702d785424c
rickjp <3741326+RickJP@users.noreply.github.com>
2020-08-28 22:14:09 +0900

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAwPUIVqQC_dbBfmxsyhVfLCRjNClYXHHc",
+  authDomain: "facebook-clone-8b3fb.firebaseapp.com",
+  databaseURL: "https://facebook-clone-8b3fb.firebaseio.com",
+  projectId: "facebook-clone-8b3fb",
+  storageBucket: "facebook-clone-8b3fb.appspot.com",
+  messagingSenderId: "247908755676",
+  appId: "1:247908755676:web:93f4bb153616c21a76ec19",
+  measurementId: "G-P1NB9RPKH2"
+};
